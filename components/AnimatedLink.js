73e56b7cf0123e1ac05927e86d587b44b4f6a8a7
skymf <divevesha@icloud.com>
2023-10-31 02:11:40 +0100

    e

--- components/AnimatedLink.js
+++ components/AnimatedLink.js
@@ -1,92 +1,91 @@
-"use client";
-import React, { useState, isHovered} from "react";
+import React, { useState } from "react";
 import { motion } from "framer-motion";
 
-
 export default function AnimatedLink({ title }) {
-    const [isHovered, setHovered] = useState(false);
+  const [isHovered, setHovered] = useState(false);
 
-    return (
-        <motion.div
-            onMouseEnter={() => setHovered(true)}
-            onMouseLeave={() => setHovered(false)}
-            className="relative overflow-hidden cursor-pointer">
-        <AnimatedWord title={title} animation={letterAnimation} isHovered = {isHovered} />
-        <div className="absolute top-0">
-        <AnimatedWord title={title} animation={letterAnimationTwo} isHovered = {isHovered} />
-        </div>
-        </motion.div>
-    );
+  return (
+    <motion.div
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+      className="relative overflow-hidden cursor-pointer"
+    >
+      <AnimatedWord title={title} animation={letterAnimation} isHovered={isHovered} />
+      <div className="absolute top-0">
+        <AnimatedWord title={title} animation={letterAnimationTwo} isHovered={isHovered} />
+      </div>
+    </motion.div>
+  );
 }
-    const titleAnimation = {
-    rest: {
-        transition: {
-        staggerChildren: 0.003,
-        },
+
+const titleAnimation = {
+  rest: {
+    transition: {
+      staggerChildren: 0.003,
     },
-    hover: {
-        transition: {
-        staggerChildren: 0.003,
-        },
+  },
+  hover: {
+    transition: {
+      staggerChildren: 0.003,
     },
-};
-  
-    const letterAnimation = {
-    rest: {
-        y: 0,
-    },
-    hover: {
-        y: -25,
-        transition: {
-        duration: 0.3,
-        ease: [0.6, 0.01, 0.05, 0.95],
-        type: "tween",
-        },
-    },
+  },
 };
 
-    const letterAnimationTwo = {
-    rest: {
-        y: 30,
+const letterAnimation = {
+  rest: {
+    y: 0,
+  },
+  hover: {
+    y: -25,
+    transition: {
+      duration: 0.3,
+      ease: [0.6, 0.01, 0.05, 0.95],
+      type: "tween",
     },
-    hover: {
-        y: 0,
-        transition: {
-        duration: 0.3,
-        ease: [0.6, 0.01, 0.05, 0.95],
-        type: "tween",
-      },
-    },          
-  };
-  const AnimatedLetter = ({ character, animation }) => {
-    return (
-      <motion.span
-        key={character} // Place the key prop on the topmost element in the map function
-        variants={animation}
-        className="relative inline-block whitespace-nowrap"
-      >
-        {character}
-      </motion.span>
-    );
-  };
-  
-  const AnimatedWord = ({ title, animation, isHovered }) => {
-    return (
-      <motion.span
-          variants={titleAnimation}
-          initial="rest"
-          animate={isHovered ? "hover" : "rest"}
-          className="whitespace-nowrap relative"
-      >
-      {title
-          .split("")
-          .map((character, i) =>
-              character === " " ? (
-              <span key={i}>&nbsp;</span>
-              ) : (
-                  <AnimatedLetter key={i} character={character} animation={animation} />
-              )
-          )}
-      </motion.span>
-    );
-  };
+  },
+};
+
+const letterAnimationTwo = {
+  rest: {
+    y: 30,
+  },
+  hover: {
+    y: 0,
+    transition: {
+      duration: 0.3,
+      ease: [0.6, 0.01, 0.05, 0.95],
+      type: "tween",
+    },
+  },
+};
+
+const AnimatedLetter = ({ character, animation }) => {
+  return (
+    <motion.span
+      key={character} // Place the key prop on the topmost element in the map function
+      variants={animation}
+      className="relative inline-block whitespace-nowrap"
+    >
+      {character}
+    </motion.span>
+  );
+};
+
+const AnimatedWord = ({ title, animation, isHovered }) => {
+  return (
+    <motion.span
+      variants={titleAnimation}
+      initial="rest"
+      animate={isHovered ? "hover" : "rest"}
+      className="whitespace-nowrap relative"
+    >
+      {title.split("").map((character, i) =>
+        character === " " ? (
+          <span key={i}>&nbsp;</span>
+        ) : (
+          <AnimatedLetter key={character + i} character={character} animation={animation} />
+        )
+      )}
+    </motion.span>
+  );
+};
