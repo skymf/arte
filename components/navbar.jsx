58a8053c750686415904d3ffdcdd07b765b7cb22
skymf <divevesha@icloud.com>
2023-10-31 03:20:23 +0100

    e

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -100,12 +100,13 @@
                     exit="initial"
                     className="flex flex-col h-full justify-center font-lora items-center gap-4 "
                     >
-                    {navLinks.map((link, i) => (
-  <div className="overflow-hidden" key={link.title + i}>
-    <MobileNavLink title={link.title} href={link.href} />
-  </div>
-))}
-
+                    {navLinks.map((link, i) => {
+  return (
+    <div className="overflow-hidden" key={`${link.title}-${i}`}>
+      <MobileNavLink title={link.title} href={link.href} />
+    </div>
+  );
+})}
 
                     </motion.div>
                 </div>
